212f32737c87a351ff096ead0a45ba4b4d1ce3c1
Rishabh Shairy <rishusinha8@gmail.com>
2018-01-25 00:33:31 +0530

    Game Layout Complete

--- app/src/main/java/com/example/rishabh/matchgame/OptionFragment.java
+++ app/src/main/java/com/example/rishabh/matchgame/OptionFragment.java
@@ -1,8 +1,10 @@
 package com.example.rishabh.matchgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 /**
@@ -17,6 +19,14 @@ public class OptionFragment extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.option_fragment);
         startButton=(Button)findViewById(R.id.beginButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
