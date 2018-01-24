a70d5a8a0b27a3caf7b4b2d73e00dd90671594bd
Your Name <you@example.com>
2018-01-25 00:31:47 +0530

    change in GameActivity

--- app/src/main/java/com/example/rishabh/matchgame/GameActivity.java
+++ app/src/main/java/com/example/rishabh/matchgame/GameActivity.java
@@ -2,6 +2,7 @@ package com.example.rishabh.matchgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        ImageView drag1= (ImageView)findViewById(R.id.option1);
+        ImageView drag2= (ImageView)findViewById(R.id.option2);
+        ImageView drag3= (ImageView)findViewById(R.id.option3);
+        ImageView drag4= (ImageView)findViewById(R.id.option4);
     }
 }
