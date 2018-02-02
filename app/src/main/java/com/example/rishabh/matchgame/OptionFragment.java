0bae0a957abf61f1023c06d76ae549d23364ebd6
Rishabh Shairy <rishusinha8@gmail.com>
2018-02-03 02:39:55 +0530

    New Changes for extra features

--- app/src/main/java/com/example/rishabh/matchgame/OptionFragment.java
+++ app/src/main/java/com/example/rishabh/matchgame/OptionFragment.java
@@ -1,32 +1,42 @@
 package com.example.rishabh.matchgame;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Rishabh on 1/23/2018.
  */
 
 public class OptionFragment extends AppCompatActivity {
-    Button startButton;
+    ImageButton learn,play;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.option_fragment);
-        startButton=(Button)findViewById(R.id.beginButton);
+        learn= (ImageButton) findViewById(R.id.learnButton);
+        play= (ImageButton) findViewById(R.id.playButton);
 
-        startButton.setOnClickListener(new View.OnClickListener() {
+        learn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),GameActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+
             }
         });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+
     }
 
 
